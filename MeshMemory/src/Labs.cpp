c3331d0c5dd9080aaca1dd67a7c487070748f8f1
randy <randy@10.10.10.34>
2016-09-08 21:55:53 -0600

    Mi segundo commit. Randy.

--- MeshMemory/src/Labs.cpp
+++ MeshMemory/src/Labs.cpp
@@ -41,7 +41,7 @@ int main() {
     //cout << "B = " << B.a << ',' << B.b << "\n";
     //cout << "C = " << C.a << ',' << C.b << "\n";
 
-	cout << "Hola Mundo de parte de mi!" << endl;
+	cout << "Hola Saturno!" << endl;
 
 //-----------------------------------------------------------------------------------------------------------------------
 	//cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
